b615ea24fd3fcfc5ffe9996d280dc9bf280e1361
Ulyana <lana.arfinova@gmail.com>
2023-11-20 18:32:50 +0300

    HW 1 done

--- src/HW 1/hw-1.js
+++ src/HW 1/hw-1.js
@@ -1,3 +1,22 @@
-export function x() {
-    
+function sum(a, b) {
+  return "Сумма: " + (a + b) + ". " + "Произведение: " + a * b + ".";
 }
+
+function sumLength(a, b) {
+  const sum = a + b;
+  return sum.length;
+}
+
+function sumOfNumbers(num) {
+  const hundreads = Math.floor(num / 100);
+  const dozens = Math.floor((num - hundreads * 100) / 10);
+  const units = num - hundreads * 100 - dozens * 10;
+  return hundreads + dozens + units;
+}
+function startSumOfNumbers() {
+  const num = +prompt("Введите трехзначное число: ");
+  console.log(sumOfNumbers(num));
+}
+startSumOfNumbers();
+
+module.exports = {sum, sumLength, sumOfNumbers};
--- src/HW 1/hw-1.test.js
+++ src/HW 1/hw-1.test.js
@@ -1,1 +1,13 @@
-import {} from "./hw-1"
+import {sum, sumLength, sumOfNumbers} from "./hw-1"
+
+test("Сумма и произведение 4 и 3", () => {
+    expect(sum(4, 3)).toBe("Сумма: 7. Произведение: 12.");
+  });
+  
+test("Суммарная длина двух строк abcd и efg", () => {
+    expect(sumLength("abcd", "efg")).toBe(7);
+  });
+  
+test("Сумма цифр числа 123", () => {
+    expect(sumOfNumbers(123)).toBe(6);
+  });
