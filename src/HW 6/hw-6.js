2d0840b7c0b8bc18eb8a889aa8efbfe8002861fa
Ulyana <lana.arfinova@gmail.com>
2023-11-25 16:23:37 +0300

    HW 6 done

---
+++ src/HW 6/hw-6.js
@@ -0,0 +1,23 @@
+function diff(a, b) {
+    if (a > b) {
+      return a - b;
+    } else {
+      return b - a;
+    }
+}
+
+function isWord(str) {
+    const words = str.split(" ");
+    if (words.length === 1) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+function pow(a, x) {
+    const aInx = Math.pow(a, x);
+    return aInx;
+}
+
+module.exports = {diff, isWord, pow};
